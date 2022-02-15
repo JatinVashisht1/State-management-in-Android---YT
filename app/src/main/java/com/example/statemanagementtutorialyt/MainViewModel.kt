dae2e3f8386ad11960a8a9a3dd55a3cd3d7427af
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-02-15 15:59:48 +0530

    initial commit

---
+++ app/src/main/java/com/example/statemanagementtutorialyt/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.statemanagementtutorialyt
+
+import javax.inject.Inject
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MainViewModel  @Inject constructor(): ViewModel() {
+
+}
