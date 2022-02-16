5b3e68e7efbc9f7f0a368be04ef727617b55436c
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-02-16 15:39:23 +0530

    initial project

--- app/src/main/java/com/example/statemanagementtutorialyt/MainActivity.kt
+++ app/src/main/java/com/example/statemanagementtutorialyt/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.statemanagementtutorialyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.statemanagementtutorialyt.ui.theme.StateManagementTutorialYTTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.receiveAsFlow
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -24,14 +30,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MyUi()
+                    
                 }
             }
         }
     }
 }
 
-@Composable
-fun MyUi() {
-
-}
--- app/src/main/java/com/example/statemanagementtutorialyt/MainViewModel.kt
+++ app/src/main/java/com/example/statemanagementtutorialyt/MainViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.statemanagementtutorialyt
 
-import javax.inject.Inject
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel  @Inject constructor(): ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
 
 }
